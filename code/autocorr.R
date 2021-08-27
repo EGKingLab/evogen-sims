2987e027b3322dbf842e076e6f1b743529342d58
egking <kingeg@missouri.edu>
2021-08-27 10:26:51 -0500

    Update prelim plots

--- code/autocorr.R
+++ code/autocorr.R
@@ -1,6 +1,7 @@
 library(tseries)
 library(slider)
 library(spectral)
+library(ggplot2)
 library(cowplot)
 theme_set(theme_cowplot())
 
@@ -71,6 +72,194 @@ ggsave(pall, file="../output/sim_spectral.pdf", width=8, height=4)
 #plot(mm)
 #abline(v=c(25,50,75,100))
 
+set.sims <- data.frame("pop"=c("C","C","F","F"), "sel"=c("C","F","C","F"), 
+                       "samp_id"=c("S6","S4","S301","S300"))
+
+out.diff1 <- matrix(NA,499,5)
+out.abs.diff1 <- matrix(NA,499,5)
+out.diff25 <- matrix(NA,475,5)
+out.abs.diff25 <- matrix(NA,475,5)
+
+out.abs.diff_sub <- matrix(NA,19,5)
+
+for(jj in 1:4)
+{
+pop_type <- set.sims$pop[jj]
+sel_type <- set.sims$sel[jj]
+samp_id <- set.sims$samp_id[jj]
+output <- readRDS(file = paste0("../output/Sim_Output_S",sel_type,"_P",pop_type,"_",samp_id,".rds"))
+ff1 <- output$freqs[seq(1,500, by =1),]
+
+  dd <- diff(ff1, lag=1)
+  out.abs.diff1[,jj] <- rowMeans(abs(dd))
+  out.diff1[,jj] <- rowMeans(dd)
+  dd <- diff(ff1, lag=25)
+  out.abs.diff25[,jj] <- rowMeans(abs(dd))
+  out.diff25[,jj] <- rowMeans(dd)
+  
+  ff2 <- output$freqs[seq(1,500, by = 25),]
+  dd <- diff(ff2, lag=1)
+  out.abs.diff_sub[,jj] <- rowMeans(abs(dd))
+}
+
+
+output.N <- readRDS(file="../output/Null_Sim_Output.rds")
+ff1 <- output.N[seq(11,510,by=1),]
+dd <- diff(ff1, lag=1)
+out.abs.diff1[,5] <- rowMeans(abs(dd))
+out.diff1[,5] <- rowMeans(dd)
+dd <- diff(ff1, lag=25)
+out.abs.diff25[,5] <- rowMeans(abs(dd))
+out.diff25[,5] <- rowMeans(dd)
+ff2 <- output$freqs[seq(11,510, by = 25),]
+dd <- diff(ff2, lag=1)
+out.abs.diff_sub[,5] <- rowMeans(abs(dd))
+
+
+plot(out.abs.diff1[,3])
+points(out.abs.diff1[,2], col='blue')
+points(out.abs.diff1[,1],col='green')
+points(out.abs.diff1[,4],col='red')
+points(out.abs.diff1[,5],col='yellow')
+
+
+plot(out.diff1[,3])
+points(out.diff1[,2], col='blue')
+points(out.diff1[,1],col='green')
+points(out.diff1[,4],col='red')
+
+plot(out.abs.diff25[,3])
+points(out.abs.diff25[,2], col='blue')
+points(out.abs.diff25[,1],col='green')
+points(out.abs.diff25[,4],col='red')
+
+plot(out.diff25[,3])
+points(out.diff25[,2], col='blue')
+points(out.diff25[,1],col='green')
+points(out.diff25[,4],col='red')
+points(out.diff25[,5],col='yellow')
+
+plot(out.abs.diff_sub[,4],ylim=c(0,0.05),type='l')
+lines(out.abs.diff_sub[,1], col='blue')
+lines(out.abs.diff_sub[,2],col='green')
+lines(out.abs.diff_sub[,3],col='red')
+lines(out.abs.diff_sub[,5],col='yellow')
+
+dd1 <- data.frame("Generation"= rep(seq(1,499),5), 
+                 "pop"=rep(c("C","C","F","F","C"), each=499),
+                 "sel"=rep(c("C","F","C","F","N"),each=499),
+                 "AbsoluteDifference"=c(out.abs.diff1[,1],
+                                         out.abs.diff1[,2],
+                                         out.abs.diff1[,3],
+                                         out.abs.diff1[,4],
+                                         out.abs.diff1[,5]))
+
+dd25 <- data.frame("Generation"= rep(seq(1,475),5), 
+                  "pop"=rep(c("C","C","F","F","C"), each=475),
+                  "sel"=rep(c("C","F","C","F","N"),each=475),
+                  "AverageDifference"=c(out.diff25[,1],
+                                          out.diff25[,2],
+                                          out.diff25[,3],
+                                          out.diff25[,4],
+                                          out.diff25[,5]))
+
+ddsub <- data.frame("Generation"= rep(seq(1,19),5), 
+                  "pop"=rep(c("C","C","F","F","C"), each=19),
+                  "sel"=rep(c("C","F","C","F","N"),each=19),
+                  "AbsoluteDifference"=c(out.abs.diff_sub[,1],
+                                         out.abs.diff_sub[,2],
+                                         out.abs.diff_sub[,3],
+                                         out.abs.diff_sub[,4],
+                                         out.abs.diff_sub[,5]))
+
+pplot1 <- ggplot(dd1,aes(Generation, AbsoluteDifference, color=sel, pch=pop)) +
+  geom_point(alpha=1/2) +
+  scale_color_manual(values=c("coral","grey30","steelblue")) +
+  ylab("Mean Absolute Difference") +
+  annotate("text",label="simulated \ndatasets", x=455, y =0.0073)
+pplot1
+
+pplot1 <- ggplot(dd1,aes(Generation, AbsoluteDifference, color=pop, linetype=sel)) +
+  geom_line() +
+  scale_color_manual(values=c("coral","grey30","steelblue")) +
+  ylab("Mean Absolute Difference")
+pplot1
+
+pplotsub <- ggplot(ddsub,aes(Generation*25, AbsoluteDifference, color=sel, linetype=pop)) +
+  geom_line() +
+  scale_color_manual(values=c("coral","grey30","steelblue")) +
+  ylab("Mean Absolute Difference") 
+pplotsub
+
+pplot25 <- ggplot(dd25,aes(Generation, AverageDifference, color=sel, pch=pop)) +
+  geom_point(alpha=1/2) +
+  scale_color_manual(values=c("coral","grey30","steelblue")) +
+  ylab("Mean Difference")
+pplot25
+
+emp.diff <- readRDS(file="../output/emp_data_diff.Rds")
+emp.diff$pop <- "Large"
+emp.small <- readRDS(file="../output/emp_data_small_diffs.Rds")
+emp.small$pop <- "Small"
+
+emp.diff <- rbind(emp.diff, emp.small)
+emp.means <- emp.diff %>% 
+  group_by(samp, Generations, pop) %>%
+  summarise("ABSD" = mean(AbsoluteDifference))
+
+empp <- emp.diff %>% filter(samp %in% c("maf_10_","maf_11_","freq_YEE_0112_04_02_","freq_YEE_0112_04_03_")) %>%
+  ggplot(aes(Generations, AbsoluteDifference, group=sample_chromosome, shape=pop, color=samp)) +
+  geom_point(alpha=1/2,position = position_jitter(width = 0.25), size=1.8) + 
+  xlab("Generation Span") +
+  ylab("Mean Absolute Difference") +
+  scale_x_continuous(breaks=c(1,2), labels=c("G180_G360","G360_G540")) +
+  guides(color="none") +
+  annotate("text",label="empirical yeast \ndataset", x=1.2, y =0.25)
+empp
+
+empm <- emp.means %>% 
+  filter(samp %in% c("freq_YEE_0112_04_01_","freq_YEE_0112_04_02_",
+                     "freq_YEE_0112_04_03_","freq_YEE_0112_04_10_",
+                     "freq_YEE_0112_04_11_","freq_YEE_0112_04_12_",
+                     "maf_3_","maf_7_",
+                     "maf_8_","maf_9_",
+                     "maf_10_","maf_11_",
+                     "maf_12_")) %>%
+  ggplot(aes(Generations, ABSD, color=pop)) +
+  geom_point(alpha=1/2,position = position_jitter(width = 0.25)) + 
+  xlab("Generation") +
+  ylab("Mean Absolute Difference") +
+  scale_x_continuous(breaks=c(1,2), labels=c("timept1","timept2"))
+#theme(legend.position = "none") +
+#ylim(0,0.15)
+empm
+
+pall <- plot_grid(pplot1, empp, ncol=2, labels=c("a.","b."))
+ggsave(pall, file="../output/pop_abs_diffs.pdf", width=8, height=3.5)
+
+
+empp <- emp.diff %>% filter(samp %in% c("freq_YEE_0112_04_01_","freq_YEE_0112_04_02_","freq_YEE_0112_04_03_","freq_YEE_0112_04_10_")) %>%
+  ggplot(aes(Generations, AbsoluteDifference, group=sample_chromosome, color=samp)) +
+  geom_line(alpha=1/2) + 
+  xlab("Generation") +
+  ylab("Mean Absolute Difference") +
+  theme(legend.position = "none") +
+#ylim(0,0.15)
+empp
+
+set.sims <- data.frame("pop"=c("C","C","F","F"), "sel"=c("C","F","C","F"), 
+                       "samp_id"=c("S6","S4","S301","S300"))
+
+
+jj<-2
+
+  pop_type <- set.sims$pop[jj]
+  sel_type <- set.sims$sel[jj]
+  samp_id <- set.sims$samp_id[jj]
+  output <- readRDS(file = paste0("../output/Sim_Output_S",sel_type,"_P",pop_type,"_",samp_id,".rds"))
+  ff1 <- output$freqs[seq(1,500, by =1),]
+  
+  
 dd.mat.abs <- vector(mode='list', length=(nrow(ff1)-1))
 dd.mat <- vector(mode='list', length=(nrow(ff1)-1))
 for(jj in 1:(nrow(ff1)-1))
--- code/empirical_set.R
+++ code/empirical_set.R
@@ -40,6 +40,43 @@ emp.set.all$sample_chromosome <- paste0(emp.set.all$samp,emp.set.all$chr)
 saveRDS(emp.set.all, file="../output/emp_data_spec.Rds")
 
 
+
+
+emp.set.all <- data.frame("samp"=character(length=0),
+                          "chr" = character(length=0),
+                          "Generations"=numeric(length=0), "AbsoluteDifference"= numeric(length=0))
+for(aa in samps)
+{
+  for(bb in arms)
+  {
+    
+    M_rep <- MM %>% 
+      select("chr","pos",starts_with("ancmaf")|starts_with(aa)) %>%
+      filter(chr==bb)
+    
+    Mspec <- as.matrix(t(M_rep[3:19]))
+    
+    dd <- diff(Mspec, lag=1)
+    
+emp.set <- data.frame("samp"=rep(aa, length=nrow(dd)),
+                      "chr" = rep(bb,length=nrow(dd)),
+                      "Generations"=seq(1:16), "AbsoluteDifference"= rowMeans(abs(dd)))
+emp.set.all <- rbind(emp.set.all, emp.set)
+cat(aa,"\t",bb,"\n")
+  }
+  
+}
+
+emp.set.all$sample_chromosome <- paste0(emp.set.all$samp,emp.set.all$chr)
+
+saveRDS(emp.set.all, file="../output/emp_data_diff.Rds")
+
+
+    
+
+
+
+
 emp.set.all %>% filter(freq <= 200) %>%
   ggplot(aes(freq, spec, group=sample_chromosome, color=samp)) +
   geom_line(alpha=1/2) #+ 
@@ -83,3 +120,92 @@ Long_genes %>%
   ggplot(aes(Generation, AlleleFrequency, group=pos)) +
   geom_line(alpha=1/20) +
   scale_x_discrete(labels=as.character(seq(0,16)))
+
+
+
+#match small set
+
+samps <- paste0("maf_",seq(1,12),"_")
+arms <- unique(MM$chr)
+
+emp.set.all <- data.frame("samp"=character(length=0),
+                          "chr" = character(length=0),
+                          "Generations"=numeric(length=0), "AbsoluteDifference"= numeric(length=0))
+for(aa in samps)
+{
+  for(bb in arms)
+  {
+    
+    M_rep <- MM %>% 
+      select("chr","pos",starts_with("ancmaf")|starts_with(aa)) %>%
+      filter(chr==bb)
+    
+    foc.cols <- paste0(aa, c("6","12","18"))
+    
+    
+    Mspec <- as.matrix(t(M_rep[,foc.cols]))
+    
+    dd <- diff(Mspec, lag=1)
+    
+    emp.set <- data.frame("samp"=rep(aa, length=nrow(dd)),
+                          "chr" = rep(bb,length=nrow(dd)),
+                          "Generations"=c(1,2), "AbsoluteDifference"= rowMeans(abs(dd)))
+    emp.set.all <- rbind(emp.set.all, emp.set)
+    cat(aa,"\t",bb,"\n")
+  }
+  
+}
+
+emp.set.all$sample_chromosome <- paste0(emp.set.all$samp,emp.set.all$chr)
+
+saveRDS(emp.set.all, file="../output/emp_data_diff.Rds")
+
+
+
+
+
+load(file="../output/small_sex.rda")
+MS <- small_sex %>% 
+  select("CHROM","POS",starts_with("freq_YEE_0112_04"))
+
+
+samps <- c("freq_YEE_0112_04_01_",
+           "freq_YEE_0112_04_02_",
+           "freq_YEE_0112_04_03_",
+           "freq_YEE_0112_04_10_",
+           "freq_YEE_0112_04_11_",
+           "freq_YEE_0112_04_12_")
+
+arms <- unique(MS$CHROM)
+
+
+emp.set.all <- data.frame("samp"=character(length=0),
+                          "chr" = character(length=0),
+                          "Generations"=numeric(length=0), "AbsoluteDifference"= numeric(length=0))
+for(aa in samps)
+{
+  for(bb in arms)
+  {
+    
+    M_rep <- MS %>% 
+      select("CHROM","POS",starts_with(aa)) %>%
+      filter(CHROM==bb)
+    foc.cols <- paste0(aa, c("06","12","18"))
+    Mspec <- as.matrix(t(M_rep[,foc.cols]))
+    
+    dd <- diff(Mspec, lag=1)
+    
+    Gens <- unlist(lapply(str_split(colnames(M_rep[,foc.cols]),"_"), function(x) x[6]))
+    
+    emp.set <- data.frame("samp"=rep(aa, length=nrow(dd)),
+                          "chr" = rep(bb,length=nrow(dd)),
+                          "Generations"=c(1,2), "AbsoluteDifference"= rowMeans(abs(dd)))
+    emp.set.all <- rbind(emp.set.all, emp.set)
+    cat(aa,"\t",bb,"\n")
+  }
+  
+}
+
+emp.set.all$sample_chromosome <- paste0(emp.set.all$samp,emp.set.all$chr)
+
+saveRDS(emp.set.all, file="../output/emp_data_small_diffs.Rds")
