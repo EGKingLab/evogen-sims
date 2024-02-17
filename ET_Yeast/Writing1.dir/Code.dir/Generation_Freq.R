306c51d767624b7cd6620badbfd4b6803b32c213
etb68 <etb68@mail.missouri.edu>
2024-02-17 15:05:36 -0600

    changed some files including More_Alleles file

--- ET_Yeast/Writing1.dir/Code.dir/Generation_Freq.R
+++ ET_Yeast/Writing1.dir/Code.dir/Generation_Freq.R
@@ -10,10 +10,11 @@ library(purrr)
 library(doParallel)
 theme_set(theme_cowplot())
 mytheme <- function(){
-  theme(axis.title = element_text(face = "bold"),
-        axis.text = element_text(face = "italic"),
-        plot.title = element_text(hjust = 0.5),
-        legend.position = "none")
+    theme_set(theme_cowplot())+
+    theme(axis.text = element_text(size = 12),
+          axis.line = element_line(size = 2),
+          axis.title = element_text(face = "bold"),
+          legend.position = "none")
 }
 
 ####################
@@ -34,27 +35,39 @@ gen_plotting_function <- function(dirpath, pattern, ncol_plots = 2, label_size =
     gen <- as.numeric(str_extract(file, "(?<=Gen)\\d+"))
     
     read.csv(file, header = TRUE) %>% 
-      select(Generation, Position, Frequency) %>% 
+      select(Generation, Position, Frequency, Effect) %>% 
       mutate(Position = factor(Position),
              herit = rep(herit, n()),
              loci = rep(loci, n()),
              sd = rep(sd, n()),
-             gen = rep(gen, n())
-      )
-  }) %>% 
-    mutate(h2_sd = paste("h2 = ", herit, " ", "sd = ", sd, " ", "generations = ", gen, sep = ""))
+             gen = rep(gen, n()),
+             Pos_Effect = paste("Position = ", Position, " Effect = ", Effect),
+             h2_sd = paste("h2 = ", herit, " sd = ", sd, " generations = ", gen, sep = ""))
+  })
+  
+  #   # Select 30 random positions
+  set.seed(12345)
+  
+  selected_positions <- combined_data %>%
+    group_by(loci) %>%
+    sample_n(size = min(30, n()), replace = F) %>%
+    pull(Pos_Effect)
+  
+
+    # Filter the data for the selected positions
+    combined_data <- combined_data %>%
+      filter(Pos_Effect %in% selected_positions)
   
   # Plotting
   plots <- combined_data %>%
     group_by(loci) %>%
     nest() %>%
     mutate(plot = map(data, ~{
-      ggplot(.x, aes(Generation, Frequency, group = Position, color = Position)) +
+      ggplot(.x, aes(Generation, Frequency, group = Pos_Effect, color = Pos_Effect)) +
         geom_line(linewidth = 0.1) +
         facet_wrap(~h2_sd, ncol = 4) +
         ylim(min = 0, max = 1) +
-        theme_bw() +
-        theme(legend.position = "none")
+        mytheme()
     })) %>%
     pull(plot)
   
@@ -69,42 +82,129 @@ gen_plotting_function <- function(dirpath, pattern, ncol_plots = 2, label_size =
 
 
 
-# 
-# dirpath <- "~/YeastProj.dir/evogen-sims/ET_Yeast/output.dir/Selection_Models/WF.dir/LinFS.dir/"
-# pattern <- "^genome1_n(1|10|100|300)_H(0\\.1|0\\.8)SD(1|4)Gen(10|30)\\.csv$"
-# files <- list.files(dirpath, pattern, full.names = TRUE)
-# 
-# combined_data <- map_dfr(files, ~{
-#   file <- .x
-#   herit <- as.numeric(str_extract(file, "(?<=H)0\\.\\d+"))
-#   loci <- as.numeric(str_extract(file, "(?<=_n)\\d+"))
-#   sd <- as.numeric(str_extract(file, "(?<=SD)\\d+"))
-#   gen <- as.numeric(str_extract(file, "(?<=Gen)\\d+"))
+#################################################################
+###Function to select only 30 loci for better visualization #####
+#################################################################
+
+gen2_plotting_function <- function(dirpath, pattern, ncol_plots = 2, label_size = 14, label_x = 0.5, label_y = 1) {
+  
+  # Data Processing
+  files <- list.files(dirpath, pattern, full.names = TRUE)
+  
+  combined_data <- map_dfr(files, ~{
+    file <- .x
+    herit <- as.numeric(str_extract(file, "(?<=H)0\\.\\d+"))
+    loci <- as.numeric(str_extract(file, "(?<=_n)\\d+"))
+    sd <- as.numeric(str_extract(file, "(?<=SD)\\d+"))
+    gen <- as.numeric(str_extract(file, "(?<=Gen)\\d+"))
+    
+    read.csv(file, header = TRUE) %>% 
+      select(Generation, Position, Frequency, Effect) %>% 
+      mutate(Position = factor(Position),
+             herit = rep(herit, n()),
+             loci = rep(loci, n()),
+             sd = rep(sd, n()),
+             gen = rep(gen, n()),
+             Pos_Effect = paste("Position = ", Position, " Effect = ", Effect),
+             h2_sd = paste("h2 = ", herit, " sd = ", sd, " generations = ", gen, sep = ""))
+  })
+  
+  # Ensure unique Pos_Effects are sampled correctly
+  set.seed(12345)
+  
+  selected_positions <- combined_data %>%
+    group_by(loci) %>%
+    sample_n(size = min(30, n()), replace = F) %>%
+    pull(Pos_Effect)
+  
+  
+  # Filter the data for the selected positions
+  combined_data <- combined_data %>%
+    filter(Pos_Effect %in% selected_positions)
+  
+  # Plotting with ggplotly for interactivity
+  plots <- combined_data_filtered %>%
+    group_by(loci) %>%
+    nest() %>%
+    mutate(plot = map(data, ~{
+      p <- ggplot(.x, aes(Generation, Frequency, group = Pos_Effect, color = Pos_Effect)) +
+        geom_line(linewidth = 0.1) +
+        facet_wrap(~h2_sd, ncol = 4) +
+        ylim(0, 1) +
+        theme_minimal() +
+        theme(legend.position = "none") +
+        labs(x = "Generation", y = "Frequency")
+      ggplotly(p)
+    })) %>%
+    pull(plot)
+  
+  # Since ggplotly plots are interactive, they can't be combined using cowplot.
+  # Consider returning a list of plots or displaying them individually.
+  
+  return(plots)
+}
+
+
+# gen2_plotting_function <- 
+#   function(dirpath, pattern, ncol_plots = 2, 
+#            label_size = 14, label_x = 0, 
+#            label_y = 1) {
 #   
-#   read.csv(file, header = TRUE) %>% 
-#     select(Generation, Position, Frequency) %>% 
-#     mutate(Position = factor(Position),
-#            herit = rep(herit, n()),
-#            loci = rep(loci, n()),
-#            sd = rep(sd, n()),
-#            gen = rep(gen, n())
-#     )
-# }) %>% 
-#   mutate(h2_sd = paste("h2 = ", herit, " ", "sd = ", sd, " ", "generations = ", gen, sep = ""))
-# 
-# plots <- combined_data %>%
-#   group_by(loci) %>%
-#   nest() %>%
-#   mutate(plot = map(data, ~{
-#     ggplot(.x, aes(Generation, Frequency, group = Position, color = Position)) +
-#       geom_line(linewidth = 0.1) +
-#       facet_wrap(~h2_sd, ncol = 4) +
-#       ylim(min = 0, max = 1) +
-#       theme_bw() +
-#       theme(legend.position = "none")
-#   })) %>%
-#   pull(plot)
-# 
-# cowplot::plot_grid(plotlist = plots, ncol = 2, align = 'v', 
-#                    labels = "AUTO", label_size = 14, 
-#                    label_x = 0, label_y = 1)
+#   # Data Processing
+#   files <- list.files(dirpath, pattern, full.names = TRUE)
+#   
+#   combined_data <- map_dfr(files, ~{
+#     file <- .x
+#     herit <- as.numeric(str_extract(file, "(?<=H)0\\.\\d+"))
+#     loci <- as.numeric(str_extract(file, "(?<=_n)\\d+"))
+#     sd <- as.numeric(str_extract(file, "(?<=SD)\\d+"))
+#     gen <- as.numeric(str_extract(file, "(?<=Gen)\\d+"))
+#     
+#     read.csv(file, header = TRUE) %>% 
+#       select(Generation, Position, Frequency, Effect) %>% 
+#       mutate(Position = factor(Position),
+#              herit = rep(herit, n()),
+#              loci = rep(loci, n()),
+#              sd = rep(sd, n()),
+#              gen = rep(gen, n()),
+#              Pos_Effect = paste("Position = ", Position, " Effect = ", Effect)
+#       )
+#   }) %>% 
+#     mutate(h2_sd = paste("h2 = ", herit, " ", "sd = ", sd, " ", 
+#                          "generations = ", gen, sep = ""))
+#   
+#   # Select 30 random positions
+#   set.seed(12345)
+#   selected_positions <- 
+#     sample(combined_data$Pos_Effect, 
+#            size = min(30, nrow(combined_data)), 
+#            replace = T)
+#   
+#   # Filter the data for the selected positions
+#   combined_data <- combined_data %>% 
+#     filter(Pos_Effect %in% selected_positions)
+#   
+#   # Plotting
+#   plots <- combined_data %>%
+#     group_by(loci) %>%
+#     nest() %>%
+#     mutate(plot = map(data, ~{
+#       ggplot(.x, aes(Generation, Frequency, 
+#                      group = Pos_Effect, color = Pos_Effect)) +
+#         geom_line(linewidth = 0.1) +
+#         facet_wrap(~h2_sd, ncol = 4) +
+#         ylim(min = 0, max = 1) +
+#         theme_bw() +
+#         theme(legend.position = "none")
+#     })) %>%
+#     pull((plot))
+#   
+#   # Combine all plots using plot_grid
+#   combined_plot <- 
+#     cowplot::plot_grid(plotlist = (plots), ncol = ncol_plots, align = 'v', 
+#                                       labels = "AUTO", label_size = label_size, 
+#                                       label_x = label_x, label_y = label_y)
+#   
+#   return(combined_plot)
+# }
+################################################################################
