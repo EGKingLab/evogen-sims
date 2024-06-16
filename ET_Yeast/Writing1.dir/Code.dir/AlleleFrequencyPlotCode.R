afb0ba6f496ca1dc2e2eba85e204a0e4d0b50fb8
etb68 <etb68@umsystem.edu>
2024-06-15 20:23:19 -0500

    updates of qmds

--- ET_Yeast/Writing1.dir/Code.dir/AlleleFrequencyPlotCode.R
+++ ET_Yeast/Writing1.dir/Code.dir/AlleleFrequencyPlotCode.R
@@ -203,7 +203,7 @@ process_files <- function(dirpath, pattern, plot_type) {
         ggplot(aes(Generation, Frequency, group = postion_effect_init,
                    color = postion_effect_init))+#ostion_effect_init
         geom_line(linewidth = 0.2)+
-        facet_wrap(~h2_sd, ncol = 2)+
+        facet_wrap(~h2_sd, ncol = 4)+
         ylim(min = 0, max = 1)+
         theme_bw()+
         theme(legend.position = "none")
