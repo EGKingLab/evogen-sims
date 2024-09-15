ed305604793f503cce2d84ddf64ca3af03477bbb
etb68 <etb68@umsystem.edu>
2024-09-15 11:03:00 -0500

    spectral edits

--- ET_Yeast/Writing1.dir/Code.dir/ComboSpec.R
+++ ET_Yeast/Writing1.dir/Code.dir/ComboSpec.R
@@ -1,3 +1,109 @@
+# run2_fft_analysis <- function(dirpath, pattern, spectrum_span = 2) {
+#   library(tidyr)
+#   library(forcats)
+#   library(dplyr)
+#   library(tidyselect)
+#   library(ggplot2)
+#   library(cowplot)
+#   library(lubridate)
+#   library(future)
+#   library(future.apply)
+#   theme_set(theme_cowplot())
+#   
+#   # Define a custom theme function
+#   mytheme <- theme(axis.title = element_text(face = "bold"),
+#                    axis.text = element_text(face = "italic"),
+#                    plot.title = element_text(hjust = 0.5),
+#                    legend.position = "none",
+#                    axis.text.x = element_text(face = "bold", size = 12),
+#                    axis.text.y = element_text(face = "bold", size = 12))
+#   
+#   # Create the directory to save images if it doesn't exist
+#   last_dir <- basename(normalizePath(dirpath))
+#   save_dir <- file.path("myimages", last_dir)
+#   if (!dir.exists(save_dir)) {
+#     dir.create(save_dir, recursive = TRUE)
+#   }
+#   
+#   # Pattern to match all specified file formats
+#   files <- list.files(dirpath, pattern = pattern, full.names = TRUE)
+#   
+#   # Extract information from file names
+#   files_df <- data.frame(file_name = files) %>%
+#     mutate(
+#       myloci_full = str_extract(file_name, "genome\\d+_n\\d+"),
+#       myGen_full = str_extract(file_name, "Gen\\d*"),
+#       mySD_full = str_extract(file_name, "SD\\d*"),
+#       myHert_full = str_extract(file_name, "(?<=H)\\d*\\.?\\d*"),
+#       myloci = str_remove(myloci_full, "genome\\d+_n"),
+#       myGen = str_remove(myGen_full, "Gen"),
+#       mySD = str_remove(mySD_full, "SD"),
+#       myHert = myHert_full,
+#       loci_Gen = paste(myloci, myGen, sep="_"),
+#       SD_H2 = paste("SD =", mySD, "H =", myHert)
+#     ) %>%
+#     select(-myloci_full, -myGen_full, -mySD_full, -myHert_full)
+#   
+#   all_results <- data.frame()
+#   
+#   # Process each file
+#   for (i in seq_along(files_df$file_name)) {
+#     file_name <- files_df$file_name[i]
+#     loci_Gen <- files_df$loci_Gen[i]
+#     SD_H2 <- files_df$SD_H2[i]
+#     
+#     file <- read.csv(file_name) %>%
+#       select(-Origin, -Effect) %>%
+#       pivot_wider(names_from = Position, values_from = Frequency, 
+#                   values_fill = list(Frequency = 0))
+#     
+#     ff1 <- file[, -1] # Exclude non-FFT columns
+#     
+#     # Apply FFT and calculate spectrum
+#     ttest <- spectrum(ff1, spans = spectrum_span, plot = FALSE)
+#     
+#     out.spect <- if (is.matrix(ttest$spec)) {
+#       rowMeans(ttest$spec)
+#     } else {
+#       ttest$spec
+#     }
+#     
+#     # Prepare results for plotting
+#     dd <- data.frame(
+#       Frequency = rep(ttest$freq),
+#       File = basename(file_name),
+#       spec = out.spect,
+#       loci_Gen = loci_Gen,
+#       SD_H2 = SD_H2
+#     )
+#     
+#     all_results <- rbind(all_results, dd)
+#   }
+#   
+#   # Plot results with conditional filtering based on the gener interval
+#   nloci <- unique(all_results$loci_Gen)
+#   fft1 <- list()
+#   for (loci in nloci) {
+#     threshold <- ifelse(grepl("_30$", loci), 80, 
+#                         ifelse(grepl("_20$", loci), 50, 
+#                                ifelse(grepl("_10$", loci), 40, 80)))
+#     fft1[[loci]] <- all_results %>%
+#       filter(loci_Gen == loci) %>%
+#       mutate(Frequency = 1 / Frequency) %>%
+#       filter(Frequency < threshold) %>%
+#       ggplot(aes(Frequency, spec, color = File)) + 
+#       geom_line(linewidth = 1.1) +
+#       facet_wrap(~SD_H2, scales = "free_y") + 
+#       xlab("Periodicity") +
+#       ylab("Spectral Density") +
+#       mytheme
+#     
+#     # Save the plot
+#     ggsave(filename = file.path(save_dir, paste0("Spectral_", loci, ".png")), plot = fft1[[loci]])
+#   }
+#   
+#   return(fft1)
+# }
 run2_fft_analysis <- function(dirpath, pattern, spectrum_span = 2) {
   library(tidyr)
   library(forcats)
@@ -10,13 +116,23 @@ run2_fft_analysis <- function(dirpath, pattern, spectrum_span = 2) {
   library(future.apply)
   theme_set(theme_cowplot())
   
-  # Define a custom theme function
-  mytheme <- theme(axis.title = element_text(face = "bold"),
-                   axis.text = element_text(face = "italic"),
-                   plot.title = element_text(hjust = 0.5),
-                   legend.position = "none",
-                   axis.text.x = element_text(face = "bold", size = 12),
-                   axis.text.y = element_text(face = "bold", size = 12))
+  # Define a custom theme function with bold facet labels
+  mytheme <- theme(legend.position = "none",
+                   axis.text.x = element_text(angle = 45, hjust = 1, size = 15, face = "bold"),
+                   axis.text.y = element_text(size = 15, face = "bold"),
+                   axis.line = element_line(size = 3),
+                   axis.title = element_text(size = 15, face = "bold"),
+                   strip.text = element_text(size = 15, face = "bold"),
+                   panel.spacing = unit(3, "lines")) +
+    theme(panel.grid = element_blank())
+    
+    # theme(axis.title = element_text(face = "bold"),
+    #                axis.text = element_text(face = "italic"),
+    #                plot.title = element_text(hjust = 0.5),
+    #                legend.position = "none",
+    #                axis.text.x = element_text(face = "bold", size = 12),
+    #                axis.text.y = element_text(face = "bold", size = 12),
+    #                strip.text = element_text(face = "bold", size = 14))  # Bold facet labels
   
   # Create the directory to save images if it doesn't exist
   last_dir <- basename(normalizePath(dirpath))
@@ -89,11 +205,11 @@ run2_fft_analysis <- function(dirpath, pattern, spectrum_span = 2) {
                                ifelse(grepl("_10$", loci), 40, 80)))
     fft1[[loci]] <- all_results %>%
       filter(loci_Gen == loci) %>%
-      mutate(Frequency = 1 / Frequency, spec = round((spec), 5)) %>%
+      mutate(Frequency = 1 / Frequency) %>%
       filter(Frequency < threshold) %>%
       ggplot(aes(Frequency, spec, color = File)) + 
       geom_line(linewidth = 1.1) +
-      facet_wrap(~SD_H2) + # scales = "free_y"
+      facet_wrap(~SD_H2, scales = "free_y") + 
       xlab("Periodicity") +
       ylab("Spectral Density") +
       mytheme
@@ -104,6 +220,7 @@ run2_fft_analysis <- function(dirpath, pattern, spectrum_span = 2) {
   
   return(fft1)
 }
+
 ##############################################################################
 #################### Previously used code that does not save plots ###########
 ##############################################################################
