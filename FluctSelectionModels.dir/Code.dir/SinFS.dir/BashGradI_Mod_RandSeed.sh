d68cffa17aeff8fa97b6cfa04039e35bb8f1991c
etb68 <etb68@umsystem.edu>
2025-05-17 17:49:30 -0500

    Clean simulation codes

---
+++ FluctSelectionModels.dir/Code.dir/SinFS.dir/BashGradI_Mod_RandSeed.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+max_jobs=60
+
+echo -e "=== Beginning of SLiM run with different QTLs > $(date) ===">>../../Output.dir/SinFS.dir/FS_OptSin${SLURM_JOBID}_${SLURM_ARRAY_TASK_ID}.log
+
+output="/storage/hpc/group/kinglab/etb68/evogen-sims/ET_Yeast/Output.dir/SinFS.dir/genome5_100_0.5.csv"
+if [[ ! -f "$output" ]]
+then
+  echo "My file ${output} doesn't exist. Running SLiM QTLs now."
+
+#seeds=(2345 78344 11349 85732 65741)
+#replicates=(1 2 3 4 5)
+
+heritabilities=(0.1 0.5 0.8)
+stdvs=(1 2 3 4)
+
+#creating an associative array where keys are the loci and the values are regions
+
+declare -A loci_to_regions
+loci_to_regions=( [1]=610140 [10]=61014 [70]=8716 [100]=6101 [300]=2033  )
+
+declare -A generations_to_ranges
+generations_to_ranges=( [10]=101 [20]=51 [30]=34 )
+
+  # Generate 30 unique seeds using awk for a uniform distribution (range: 4000 to 7000)
+  seeds=($(awk -v seed=12345 'BEGIN { srand(seed); for (i=1; i<=30; i++) printf "%d ", int(4000 + rand() * (7000 - 4000)); }')) # seed = "$fixed_seed"
+
+  # Mapping seeds to replicates
+  declare -A seeds_to_replicates
+  for i in {0..29}; do
+    seeds_to_replicates[${seeds[$i]}]=$((i+1))
+  done
+
+for seed in "${!seeds_to_replicates[@]}"; do
+    repl=${seeds_to_replicates[$seed]}
+  for h in ${heritabilities[@]}
+  do
+    for SD in ${stdvs[@]}
+    do
+      for loci in "${!loci_to_regions[@]}"
+      do
+        for gen in "${!generations_to_ranges[@]}"
+        do
+          repl=${seeds_to_replicates[$seed]}
+          region=${loci_to_regions[$loci]}
+          rang=${generations_to_ranges[$gen]}
+          
+          # Wait for free slot
+          while (( $(jobs | wc -l) >= max_jobs )); do sleep 1; done
+          
+	  {
+            slim -d seed=$seed -d repl=$repl -d loci=$loci -d region=$region -d h=$h -d gen=$gen -d rang=$rang -d SD=$SD GradI_Mod.slim
+          } &
+        done
+      done
+    done
+  done
+done
+
+wait
+
+else
+  echo "All is well, Boss.  The ${output} file is there."
+fi
+total_end_time=$(date +%s)
+total_runtime=$((total_end_time - total_start_time))
+echo "=== Finished! End of SLiM QTLs fluctuating 2 sinusoidal run > $(date). Total runtime: $total_runtime seconds ===" >>../../Output.dir/SinFS.dir/FS_OptSin${SLURM_JOBID}_${SLURM_ARRAY_TASK_ID}.log
